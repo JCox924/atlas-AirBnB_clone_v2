c3ccea1b69a935db147166f254d1898cf1ee17b9
JCox924 <joshua.cox@atlasschool.com>
2024-06-30 22:26:07 -0500

    Added the SQL setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,17 @@
+-- Create the database hbnb_dev_db if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user hbnb_dev with password hbnb_dev_pwd if it doesn't already exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Revoke all privileges on the database hbnb_dev_db_fake from the hbnb_dev user
+REVOKE ALL PRIVILEGES ON hbnb_dev_db_fake.* FROM 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush the privileges to ensure that all changes take effect immediately
+FLUSH PRIVILEGES;
