934a4331cc7e10efda613a0d4ae403a9d6a84939
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-07-01 07:50:19 -0500

    error still

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,20 @@
--- Create the database 'hbnb_dev_db' if it doesn't already exist
+-- Create the database hbnb_dev_db if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Drop the user 'hbnb_dev' if it exists, to ensure a clean slate
-DROP USER IF EXISTS hbnb_dev@localhost;
+-- Create the user hbnb_dev with password hbnb_dev_pwd if it doesn't already exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Refresh the list of privileges to ensure changes take effect
-FLUSH PRIVILEGES;
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Create a new user 'hbnb_dev' with the specified password, if the user doesn't already exist
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+-- Revoke all privileges on the database hbnb_dev_db_fake from the hbnb_dev user (just in case this was a typo or error)
+REVOKE ALL PRIVILEGES ON hbnb_dev_db_fake.* FROM 'hbnb_dev'@'localhost';
 
--- Grant all privileges on the 'hbnb_dev_db' database to the 'hbnb_dev' user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Revoke all privileges on the performance_schema database from the hbnb_dev user
+REVOKE ALL PRIVILEGES ON performance_schema.* FROM 'hbnb_dev'@'localhost';
 
--- Grant SELECT privileges on the 'performance_schema' to the 'hbnb_dev' user
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush the privileges to ensure that all changes take effect immediately
+FLUSH PRIVILEGES;
