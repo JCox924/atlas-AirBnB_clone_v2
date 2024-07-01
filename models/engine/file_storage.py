077c3243b974c7f6b8f49ab976714249858297af
JCox924 <joshua.cox@atlasschool.com>
2024-07-01 00:15:10 -0500

    updated function to_dict in BaseModel to properly covert instance into dict format

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,10 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
+        temp = {}
+        for key, val in self.__objects.items():
+            temp[key] = val.to_dict()
+        with open(self.__file_path, 'w') as f:
             json.dump(temp, f)
 
     # noinspection PyUnresolvedReferences
