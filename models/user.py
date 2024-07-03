8b22c7ac62a0787c609224a2333b96e45e0a8a6f
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-07-03 11:15:45 -0500

    fixing user.py

--- models/user.py
+++ models/user.py
@@ -9,6 +9,8 @@ from models.base_model import Base
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
+    
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
