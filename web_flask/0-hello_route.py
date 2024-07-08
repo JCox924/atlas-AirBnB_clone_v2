1b6dc6449f2abb0bbf9966edb44d9327c28efe87
JCox924 <joshua.cox@atlasschool.com>
2024-07-07 19:44:54 -0500

    forgot to doc 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
+'''
+Starts Flask web app with Flask routes
+'''
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """Route that returns 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
